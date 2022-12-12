455db0b5c374352709bd3c91ce3dbeea88fc9b62
toshi-hm <toshiyahama0310@gmail.com>
2022-12-12 16:55:24 +0900

    アーティスト検索結果が0件のとき、メッセージが変わるようにした

--- resources/views/musics/selectArtist.blade.php
+++ resources/views/musics/selectArtist.blade.php
@@ -4,6 +4,14 @@
     </x-slot>
     <p class="massage">
         <!-- もし1アーティストも見つからなかった場合にエラーを出す -->
+        <?php
+        $num = count($results->artists->items);
+        if($num >= 1) {
+            echo "アーティストが" . $num . "件見つかりました。";
+        } else {
+            echo "該当するアーティストが見つかりませんでした。";
+        }
+        ?>
     </p>
     @foreach($results->artists->items as $artist)
         <form action="/comments/create/artists/albums", method="POST">
