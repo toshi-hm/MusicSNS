8241fdfd4d51cd1bd7f0595fd0e58710f9f0c607
toshi-hm <toshiyahama0310@gmail.com>
2022-12-19 14:17:12 +0900

    seederファイルでcategoryを追加した

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("categories")->insert([
+           "category_name" => "J-POP",
+           "created_at" => new DateTime(),
+           "updated_at" => new DateTime(),
+        ]);
+        DB::table("categories")->insert([
+            "created_at" => "K-POP",
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
+        ]);
+        DB::table("categories")->insert([
+            "created_at" => "Classic",
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime(),
+        ]);
+    }
+}
