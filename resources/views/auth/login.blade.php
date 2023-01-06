d7dd0618192be45314dccd517bdd5463fc9ac1f5
toshi-hm <toshiyahama0310@gmail.com>
2023-01-06 13:00:39 +0900

    ルーティング修正4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <img src="HaMusicSNS_icon.png" class="w-20 h-20 fill-current text-gray-500" />
+                <img src="{{asset('storage/HaMusicSNS_icon.png')}}" class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
 
